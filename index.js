4308d81e6f96951fe0317ff2268a45d50b53e249
dougallwebdesign <dougallwebdesign@gmail.com>
2021-03-21 21:48:27 +0100

    fixed match-btn JS but still need to tweak as all buttons are chosen

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const matchBtn = document.getElementById("match-btn");
+// single match buttons
+const matchBtn = document.getElementsByClassName("match-btn")
+
+// show match details
 const styleMatchDetails = document.getElementsByClassName("fixture-button-columns")
+
 let matchDetails = true;
 
 
-function showMatchDetails() {
-    styleMatchDetails[0].style.display = "flex"
+
+        
+
+for (let i = 0; i < matchBtn.length; i++) {
+    
+    matchBtn[i].addEventListener("click", function(){
+        console.log("clicked")
+        if (matchDetails == true) {
+            matchDetails = false
+            for (let i = 0; i < styleMatchDetails.length; i++) {
+                styleMatchDetails[i].style.display = "flex"
+            }
+                
+        } else {
+            matchDetails = true
+            for (let i = 0; i < styleMatchDetails.length; i++) {
+                styleMatchDetails[i].style.display = "none"
+            }
+        }
+    })
 }
 
-function hideMatchDetails() {
-    styleMatchDetails[0].style.display = "none"
-}
 
-matchBtn.addEventListener("click", function(){
-   if (matchDetails == true) {
-       matchDetails = false
-       showMatchDetails()
-   } else {
-       matchDetails = true
-       hideMatchDetails()
-   }
-   
-    
-    
-})
+
+
 
 
 
