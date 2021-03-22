60e3e390956ce20cb34f2f6170c20fd0b8fbf69f
dougallwebdesign <dougallwebdesign@gmail.com>
2021-03-22 08:59:07 +0100

    Solved JS match buttons: they open seperatly. Logo changed again to span

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 // single match buttons
 const matchBtn = document.getElementsByClassName("match-btn")
-
 // show match details
 const styleMatchDetails = document.getElementsByClassName("fixture-button-columns")
 
 let matchDetails = true;
 
+//functions
 
 
-        
+//Match button logic once finished do DRY
+matchBtn[0].addEventListener("click", function(){
+    if (matchDetails ==true) {
+        matchDetails = false
+        styleMatchDetails[0].style.display = "flex"
+    } else {
+        matchDetails = true
+        styleMatchDetails[0].style.display = "none"
+    }
+})
 
-for (let i = 0; i < matchBtn.length; i++) {
-    
-    matchBtn[i].addEventListener("click", function(){
-        console.log("clicked")
-        if (matchDetails == true) {
-            matchDetails = false
-            for (let i = 0; i < styleMatchDetails.length; i++) {
-                styleMatchDetails[i].style.display = "flex"
-            }
-                
-        } else {
-            matchDetails = true
-            for (let i = 0; i < styleMatchDetails.length; i++) {
-                styleMatchDetails[i].style.display = "none"
-            }
-        }
-    })
-}
+matchBtn[1].addEventListener("click", function(){
+    if (matchDetails ==true) {
+        matchDetails = false
+        styleMatchDetails[1].style.display = "flex"
+    } else {
+        matchDetails = true
+        styleMatchDetails[1].style.display = "none"
+    }
+})
+
+
 
 
 
